e70e2270ddfd0d9c460517a2c1f4d70e6a02c742
Your Name <you@example.com>
2021-05-28 11:09:45 +0900

    Articles

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from data import Articles
 app = Flask(__name__)
+articles = Articles()
 
 @app.route('/', methods=['Get' , 'POST'])
-
-@app.route('/')
 def hello_world():
-    return '<h1> Hello World! </h>'
+    return render_template('home.html')
+
+@app.route('/about',methods=['Get' , 'POST'])
+def about():
+    return render_template('about.html')
+
+@app.route('/articles',methods=['Get' , 'POST'])
+def articles():
+    return render_template('articles.html', articles = Articles())
 
 if __name__ == '__main__':
     app.run(port = 8080, debug = True ) 
