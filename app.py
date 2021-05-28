c2c9bf997e2626858e0b6fe6e3713354855c20c0
Your Name <you@example.com>
2021-05-28 17:43:23 +0900

    mysql step 1

--- app.py
+++ app.py
@@ -15,5 +15,19 @@ def about():
 def articles():
     return render_template('articles.html', articles = Articles())
 
+@app.route('/article/<id>',methods=['Get' , 'POST'])
+def article(id):
+    articles = Articles()
+    print(len(articles))
+    if (len(articles)) >= int(id):
+        article = articles[int(id)-1]
+        return render_template('article.html',article = article)
+    else:
+        return render_template('article.html',article = "No Data")  
+
+@app.route('/add_article', methods=['Get' , 'POST'])
+def add_article():
+    return render_template('add_article.html')
+
 if __name__ == '__main__':
     app.run(port = 8080, debug = True ) 
